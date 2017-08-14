0fa293e7e70321ac065323e252067b1c3e4cf1c2
Yaron Uliel <yaron@techmarketing.co.il>
2017-08-14 03:28:15 +0300

    Add tests

--- README.md
+++ README.md
@@ -13,7 +13,14 @@ chmod +x ./srt-resync.php
 And then run
 
 ```
-./srt-resync.php [start] < input.srt > output.srt
+./srt-resync.php START_SECONDS [input] > output.srt
+```
+OR
+```
+./srt-resync.php START_SECONDS < input.srt > output.srt
 ```
 
-Where start is the time of the first subtitle in seconds (e.g. for 00:02:12.032 set `start` to `142.032`) - the rest of the times in the subtitle file will be set according to the offset between the original start time and the new start time
+Where `START_SECONDS` is the time of the first subtitle in seconds 
+(e.g. for `00:02:12.032` replace `START_SECONDS` with `142.032`)
+
+the rest of the times in the subtitle file will be set according to the offset between the original start time and the new start time
--- srt-resync.php
+++ srt-resync.php
@@ -1,51 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-$fh = fopen('php://stdin', 'r');
+require_once './functions.php';
 
-if($_SERVER['argc']!=2) {
-	help();
-}
+if(php_sapi_name() == 'cli') {
 
-$start = (float)$_SERVER['argv'][1];
+	if ( $_SERVER['argc'] != 2 && $_SERVER['argc'] != 3 ) {
+		help();
+	}
 
-if(!is_numeric($start)) {
-	help();
-}
+	$file = $_SERVER['argc'] == 3 ? $_SERVER['argv'][2] : 'php://stdin';
+	$fh   = fopen( $file, 'r' );
 
-$offset = null;
+	$start = (float) $_SERVER['argv'][1];
 
-while($line = fgets($fh)) {
-	echo preg_replace_callback('/(\d{2})\:(\d{2}):(\d{2})[\,\.](\d+)/', function($matches) use (&$offset, $start) {
-		$sec = timeToS($matches);
-		if($offset === null) {
+	if ( ! is_numeric( $start ) ) {
+		help();
+	}
 
-			$offset = $start - $sec;
-		}
-
-		return secToTime($sec + $offset);
-	}, $line);
-}
-
-function secToTime($sec) {
-	$h = floor($sec / 3600);
-	$m = floor(($sec % 3600) / 60);
-	$s = floor($sec % 60);
-
-	$ms = ($sec - floor($sec)) * 1000;//str_pad(($sec - abs($sec)) * 1000, '0', 3, STR_PAD_LEFT);
-
-	return sprintf('%02d:%02d:%02d,%03d', $h, $m, $s, $ms);
-}
-
-function timeToS($parts) {
-	$sec = intval($parts[1]) * 3600 + intval($parts[2]) * 60 + intval($parts[3])
-				+ intval($parts[4]) / 1000;
-	return (float)$sec;
-}
-
-
-function help() {
-	echo 'Usage: srt-sync.php +12.23 < input.srt > output.srt';
-	echo "\n";
-	exit;
+	convertFile($fh, $start);
 }
